3dd55a4097b28a3de452d679588bf387bc933607
Patrick Silva <pfivel@gmail.com>
2024-02-02 21:35:40 -0700

    retrieving database gpu data to us in home razor page

---
+++ Controllers/GPUController.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using GPUTestAnalysisCascadingDropDown.Interfaces;
+
+namespace GPUTestAnalysisCascadingDropDown.Controllers
+{
+    public class GPUController
+    {
+        private readonly IConfiguration _configuration;
+        private IGPUService _gpuService;
+        IMapper _mapper;
+        private readonly string _connectionString;
+
+        public GPUController(IConfiguration configuration, IGPUService gpuService, IMapper mapper, string connectionString)
+        {
+            _configuration = configuration;
+            _gpuService = gpuService;
+            _mapper = mapper;
+            _connectionString = connectionString;
+        }
+    }
+}
